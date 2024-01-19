19936ca7a37720839161a44d23942dbb78f34fe4
Mariam Abbas <maabbas@noreply.learn.reboot01.com>
2024-01-19 14:55:22 +0000

    Update 'src/main.c'

--- src/main.c
+++ src/main.c
@@ -1,236 +1,104 @@
 #include "header.h"
-#include <stdbool.h>
-const char *RECORDS = "./data/records.txt";
 
-int getAccountFromFile(FILE *ptr, char name[50], struct Record *r)
-{
-    return fscanf(ptr, "%d %d %s %d %d/%d/%d %s %d %lf %s",
-                  &r->id,
-		  &r->userId,
-		  name,
-                  &r->accountNbr,
-                  &r->deposit.month,
-                  &r->deposit.day,
-                  &r->deposit.year,
-                  r->country,
-                  &r->phone,
-                  &r->amount,
-                  r->accountType) != EOF;
-}
-
-void saveAccountToFile(FILE *ptr, struct User u, struct Record r)
-{
-    fprintf(ptr, "%d %d %s %d %d/%d/%d %s %d %.2lf %s\n\n",
-            r.id,
-            u.id,
-            u.name,
-            r.accountNbr,
-            r.deposit.month,
-            r.deposit.day,
-            r.deposit.year,
-            r.country,
-            r.phone,
-            r.amount,
-            r.accountType);
-}
-
-void stayOrReturn(int notGood, void f(struct User u), struct User u)
+void mainMenu(struct User u)
 {
     int option;
-    if (notGood == 0)
-    {
-        system("clear");
-        printf("\n✖ Record not found!!\n");
-    invalid:
-        printf("\nEnter 0 to try again, 1 to return to main menu and 2 to exit:");
-        scanf("%d", &option);
-        if (option == 0)
-            f(u);
-        else if (option == 1)
-            mainMenu(u);
-        else if (option == 2)
-            exit(0);
-        else
-        {
-            printf("Insert a valid operation!\n");
-            goto invalid;
-        }
-    }
-    else
-    {
-        printf("\nEnter 1 to go to the main menu and 0 to exit:");
-        scanf("%d", &option);
-    }
-    if (option == 1)
-    {
-        system("clear");
-        mainMenu(u);
-    }
-    else
-    {
-        system("clear");
-        exit(1);
-    }
-}
-
-void success(struct User u)
-{
-    int option;
-    printf("\n✔ Success!\n\n");
-invalid:
-    printf("Enter 1 to go to the main menu and 0 to exit!\n");
+    system("clear");
+    printf("\n\n\t\t======= ATM =======\n\n");
+    printf("\n\t\t-->> Feel free to choose one of the options below <<--\n");
+    printf("\n\t\t[1]- Create a new account\n");
+    printf("\n\t\t[2]- Update account information\n");
+    printf("\n\t\t[3]- Check accounts\n");
+    printf("\n\t\t[4]- Check list of owned account\n");
+    printf("\n\t\t[5]- Make Transaction\n");
+    printf("\n\t\t[6]- Remove existing account\n");
+    printf("\n\t\t[7]- Transfer ownership\n");
+    printf("\n\t\t[8]- Exit\n");
     scanf("%d", &option);
-    system("clear");
-    if (option == 1)
+
+    switch (option)
     {
-        mainMenu(u);
-    }
-    else if (option == 0)
-    {
+    case 1:
+        createNewAcc(u);
+        break;
+    case 2:
+        // student TODO : add your **Update account information** function
+        UpdateAccountInformation(u);
+        break;
+    case 3:
+        // student TODO : add your **Check the details of existing accounts** function
+        CheckTheDetailsOfExistingAccounts(u);
+        break;
+    case 4:
+        checkAllAccounts(u);
+        break;
+    case 5:
+        // student TODO : add your **Make transaction** function
+        MakeTransaction(u);
+        break;
+    case 6:
+        // student TODO : add your **Remove existing account** function
+        RemoveExistingAccount(u);
+        break;
+    case 7:
+        // student TODO : add your **Transfer owner** function
+        TransferOwner(u);
+        break;
+    case 8:
         exit(1);
+        break;
+    default:
+        printf("Invalid operation!\n");
     }
-    else
-    {
-        printf("Insert a valid operation!\n");
-        goto invalid;
-    }
-}
+};
 
-void createNewAcc(struct User u)
+void initMenu(struct User *u)
 {
-    struct Record r;
-    struct Record cr;
-    char userName[50];
-    FILE *pf = fopen(RECORDS, "a+");
-
-noAccount:
+    int r = 0;
+    int option;
     system("clear");
-    printf("\t\t\t===== New record =====\n");
-
-    printf("\nEnter today's date(mm/dd/yyyy):");
-    scanf("%d/%d/%d", &r.deposit.month, &r.deposit.day, &r.deposit.year);
-    printf("\nEnter the account number:");
-    scanf("%d", &r.accountNbr);
-
-    while (getAccountFromFile(pf, userName, &cr))
+    printf("\n\n\t\t======= ATM =======\n");
+    printf("\n\t\t-->> Feel free to login / register :\n");
+    printf("\n\t\t[1]- login\n");
+    printf("\n\t\t[2]- register\n");
+    printf("\n\t\t[3]- exit\n");
+    while (!r)
     {
-        if (strcmp(userName, u.name) == 0 && cr.accountNbr == r.accountNbr)
+        scanf("%d", &option);
+        switch (option)
         {
-            printf("✖ This Account already exists for this user\n\n");
-            goto noAccount;
-        }
-    }
-    printf("\nEnter the country:");
-    scanf("%s", r.country);
-    printf("\nEnter the phone number:");
-    scanf("%d", &r.phone);
-    printf("\nEnter amount to deposit: $");
-    scanf("%lf", &r.amount);
-    printf("\nChoose the type of account:\n\t-> saving\n\t-> current\n\t-> fixed01(for 1 year)\n\t-> fixed02(for 2 years)\n\t-> fixed03(for 3 years)\n\n\tEnter your choice:");
-    scanf("%s", r.accountType);
-
-    saveAccountToFile(pf, u, r);
-
-    fclose(pf);
-    success(u);
-}
-
-void checkAllAccounts(struct User u)
-{
-    char userName[100];
-    struct Record r;
-
-    FILE *pf = fopen(RECORDS, "r");
-
-    system("clear");
-    printf("\t\t====== All accounts from user, %s =====\n\n", u.name);
-    while (getAccountFromFile(pf, userName, &r))
-    {
-        if (strcmp(userName, u.name) == 0)
-        {
-            printf("_____________________\n");
-            printf("\nAccount number:%d\nDeposit Date:%d/%d/%d \ncountry:%s \nPhone number:%d \nAmount deposited: $%.2f \nType Of Account:%s\n",
-                   r.accountNbr,
-                   r.deposit.day,
-                   r.deposit.month,
-                   r.deposit.year,
-                   r.country,
-                   r.phone,
-                   r.amount,
-                   r.accountType);
-        }
-    }
-    fclose(pf);
-    success(u);
-}
-
-
-
-void UpdateAccountInformation(struct User u) {
-    int accountNbr;
-    struct Record cr;
-    char userName[50];
-
-    printf("What is the account number you want to change:");
-    scanf("%d", &accountNbr);
-
-    FILE *originalFile = fopen(RECORDS, "r");
-    FILE *tempFile = fopen("temp.txt", "w");
-
-    bool recordUpdated = false;
-
-    while (getAccountFromFile(originalFile, userName, &cr))
-    {
-        if (cr.accountNbr == accountNbr)
-        {
-            printf("Which information do you want to update?\n1-> Phone number\n2-> Country\n");
-            int information;
-            scanf("%d", &information);
-
-            switch (information)
+        case 1:
+            loginMenu(u->name, u->password);
+            if (strcmp(u->password, getPassword(*u)) == 0)
             {
-            case 1:
-                int phoneNumber;
-                printf("Enter the new Phone Number:");
-                scanf("%d", &phoneNumber);
-                cr.phone = phoneNumber;
-                break;
-            case 2:
-                char country[100];
-                printf("Enter the new country:");
-                scanf("%s", country);
-                strcpy(cr.country, country);
-                break;
+                printf("\n\nPassword Match!");
             }
-
-            recordUpdated = true;
+            else
+            {
+                printf("\nWrong password!! or User Name\n");
+                exit(1);
+            }
+            r = 1;
+            break;
+        case 2:
+            // student TODO : add your **Registration** function
+            // here
+            r = 1;
+            break;
+        case 3:
+            exit(1);
+            break;
+        default:
+            printf("Insert a valid operation!\n");
         }
-
-        fprintf(tempFile, "%d %d %s %d %d/%d/%d %s %d %.2lf %s\n",
-                cr.id,
-                cr.userId,
-                userName,
-                cr.accountNbr,
-                cr.deposit.month,
-                cr.deposit.day,
-                cr.deposit.year,
-                cr.country,
-                cr.phone,
-                cr.amount,
-                cr.accountType);
     }
+};
 
-    fclose(originalFile);
-    fclose(tempFile);
-
-    if (!recordUpdated) {
-        remove("temp.txt");  // Delete the temporary file
-        printf("Account number not found.\n");
-        return;
-    }
-
-    remove(RECORDS);  // Delete the original file
-    rename("temp.txt", RECORDS);  // Rename the temporary file to the original filename
-
-    success(u);
+int main()
+{
+    struct User u;
+    
+    initMenu(&u);
+    mainMenu(u);
+    return 0;
 }
